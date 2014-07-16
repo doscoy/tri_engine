d3156faa38c35f72976584529c747fa54d099e24
KANI Tetsuro <doscoy.t@gmail.com>
2014-07-17 00:21:46 +0900

    イベントリスナクラスを削除。どんなクラスのメソッドもイベントハンドラにできる

---
+++ tri_engine/include/base/tri_pause_level.hpp
@@ -0,0 +1,14 @@
+//
+//  tri_pause_level.hpp
+//  star01
+//
+//  Created by KANI Tetsuro on 2014/07/16.
+//  Copyright (c) 2014年 KANI Tetsuro. All rights reserved.
+//
+
+#ifndef star01_tri_pause_level_hpp
+#define star01_tri_pause_level_hpp
+
+
+
+#endif
