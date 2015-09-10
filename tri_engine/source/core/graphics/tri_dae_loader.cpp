a635b5add05c1b1e716010f350352b973aab25aa
KANI Tetsuro <doscoy.t@gmail.com>
2015-09-10 23:30:50 +0900

    ローダー準備

--- tri_engine/source/core/graphics/tri_dae_loader.cpp
+++ tri_engine/source/core/graphics/tri_dae_loader.cpp
@@ -11,16 +11,31 @@ TRI_CORE_NS_BEGIN
 SubMeshDataPtr DaeLoader::load(
     const char* const path
 ) {
+    //  戻すサブメッシュ
+    SubMeshDataPtr submesh = std::make_shared<SubMeshData>();
+
+    //  daeをパース
     tinycollada::Parser parser;
     parser.parse(path);
 
     const auto& scenes = parser.scenes();
-    for (const auto& s : scenes) {
-        
-    
+    for (int i = 0; i < scenes->size(); ++i) {
+        const auto& s = scenes->at(i);
+        //  シーンからメッシュを取り出す
+        auto& meshes = s->meshes_;
+        for (int mesh_idx = 0; mesh_idx < meshes.size(); ++mesh_idx) {
+            //  メッシュを走査
+            const auto& mesh = meshes.at(mesh_idx);
+            if (!mesh->hasVertex()) {
+                //  頂点情報を持ってないメッシュはスキップ
+                continue;
+            }
+
+            //  頂点をコピー
+            submesh->vertices() = mesh->vertices().data()
+        }
     }
 
-    SubMeshDataPtr submesh = std::make_shared<SubMeshData>();
   
 
     
